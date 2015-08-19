68db9116801554aab8ff95290d1eeab462807791
Jamie Eula Evangelista <jevangelista@Jamies-MacBook-Pro.local>
2015-08-19 16:13:27 -0700

    added get fetch quake data

--- base.js
+++ base.js
@@ -4,10 +4,60 @@ var map;
 
 $(document).ready(function(){
 
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
+//Create Map
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 37.78, lng: -122.44},
     zoom: 8
   });
 
+//Fetch Quake Data
+  $.get("http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson", function(data) {
+            data.features.forEach (function (quake) {
+            var title =  quake.properties.title 
+            $("#info").append("<p>" + title + "</p>");
+      });
+
+  });
+
+/** Create Marker */
+
+var myLatLng = {lat: -25.363, lng: 131.044};
+
+  var marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    title:"Hello World!"
+});
+
+
+
 })
 
+
+/** 
+-Grabs Time
+  $.get("http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson", function(response_data) {
+            response_data.features.forEach (function (title_data) {
+	            $("#info").append("<p>" + title_data.properties.title + "</p>");
+
+      });
+
+
+data.features[0].properties.time
+
+-Grabs title 
+GLOBAL_MOCK_DATA_OBJECT.data[0].features[0].properties.title
+
+-Grabs coordinates
+GLOBAL_MOCK_DATA_OBJECT.data[0].features[0].geometry.coordinates;
+*/
+
+/** Giffaw
+$( "#button" ).click(function() {
+		  var pick = $("input").val();
+		   $.get("http://api.giphy.com/v1/gifs/search", {"q": pick, "api_key": "dc6zaTOxFJmzC"}, function(response_data) {
+		 	response_data.data.forEach (function (element) {
+		 			$(".container").append("<img src=" + element.images.fixed_height.url + ">");
+
+
+*/
---
+++ mock-data.js
@@ -0,0 +1,107 @@
+var GLOBAL_MOCK_DATA_OBJECT = {
+    "data": [
+
+{
+type: "FeatureCollection",
+metadata: {
+generated: 1440018911000,
+url: "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson",
+title: "USGS Significant Earthquakes, Past Week",
+status: 200,
+api: "1.0.17",
+count: 2
+},
+features: [
+{
+type: "Feature",
+properties: {
+mag: 4.01,
+place: "1km N of Piedmont, California",
+time: 1439819357320,
+updated: 1440012426442,
+tz: -420,
+url: "http://earthquake.usgs.gov/earthquakes/eventpage/nc72507396",
+detail: "http://earthquake.usgs.gov/earthquakes/feed/v1.0/detail/nc72507396.geojson",
+felt: 14614,
+cdi: 5.2,
+mmi: 5.77,
+alert: "green",
+status: "reviewed",
+tsunami: 0,
+sig: 767,
+net: "nc",
+code: "72507396",
+ids: ",nc72507396,us100032v0,",
+sources: ",nc,us,",
+types: ",cap,dyfi,focal-mechanism,general-link,geoserve,impact-text,losspager,moment-tensor,nearby-cities,origin,phase-data,scitech-link,shakemap,tectonic-summary,",
+nst: 268,
+dmin: 0.01185,
+rms: 0.15,
+gap: 12,
+magType: "mw",
+type: "earthquake",
+title: "M 4.0 - 1km N of Piedmont, California"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-122.2321667,
+37.8365,
+4.82
+]
+},
+id: "nc72507396"
+},
+{
+type: "Feature",
+properties: {
+mag: 6.6,
+place: "214km W of Lata, Solomon Islands",
+time: 1439624826640,
+updated: 1440012841000,
+tz: 660,
+url: "http://earthquake.usgs.gov/earthquakes/eventpage/us100032k6",
+detail: "http://earthquake.usgs.gov/earthquakes/feed/v1.0/detail/us100032k6.geojson",
+felt: null,
+cdi: null,
+mmi: 3.99,
+alert: "green",
+status: "reviewed",
+tsunami: 1,
+sig: 670,
+net: "us",
+code: "100032k6",
+ids: ",pt15227050,at00nt45mm,us100032k6,gcmt20150815074706,",
+sources: ",pt,at,us,gcmt,",
+types: ",cap,geoserve,impact-link,losspager,moment-tensor,nearby-cities,origin,phase-data,shakemap,tectonic-summary,",
+nst: null,
+dmin: 4.133,
+rms: 1.04,
+gap: 22,
+magType: "mwp",
+type: "earthquake",
+title: "M 6.6 - 214km W of Lata, Solomon Islands"
+},
+geometry: {
+type: "Point",
+coordinates: [
+163.8766,
+-10.9045,
+6.35
+]
+},
+id: "us100032k6"
+}
+],
+bbox: [
+-122.2321667,
+-10.9045,
+4.82,
+163.8766,
+37.8365,
+6.35
+]
+}
+
+    ]
+}
